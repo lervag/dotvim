1569a6e430f066234788815ae22f89b16029c8c2
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2014-10-23 09:00:32 +0200

    Added simple installscript for vim-plug

---
+++ get_vim_plug.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+echo -n "Fetching vim-plug ... "
+if [ -d bundle/vim-plug ]; then
+  echo "already fetched."
+else
+  echo "done!"
+  git clone https://github.com/junegunn/vim-plug bundle/vim-plug
+fi
+
+echo -n "Installing vim-plug ... "
+if [ -e autoload/plug.vim ]; then
+  echo "already installed."
+else
+  echo "done!"
+  mkdir -p ~/.vim/autoload
+  ln -s bundle/vim-plug/vim-plug.vim ~/.vim/autoload/vim-plug.vim
+fi
+
